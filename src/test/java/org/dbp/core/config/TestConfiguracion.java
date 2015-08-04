5d638cb19e604c3cd4792891ad02bafb196cc1be
blancoparis-tfc <blancoparis.tfc@gmail.com>
2015-08-04 20:24:04 +0200

    Configuracion básica
    Configuración basica

---
+++ src/test/java/org/dbp/core/config/TestConfiguracion.java
@@ -0,0 +1,10 @@
+package org.dbp.core.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.dbp.service")
+public class TestConfiguracion {
+
+}
